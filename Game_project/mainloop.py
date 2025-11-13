7b5905964b580ab3f0c6062ed00e0428106e963e
Chatri-otto <oto77231@gmail.com>
2025-11-13 12:38:38 +0700

    Mainloop
    ตัวฉบับแก้

--- Game_project/mainloop.py
+++ Game_project/mainloop.py
@@ -14,7 +14,13 @@ screen = pygame.display.set_mode((800, 600))
 fps = 60
 clock = pygame.time.Clock()
 
-# mc.music()
+def music():
+    pygame.mixer.init()
+    pygame.mixer.music.load("Music/Theme1.mp3")
+    pygame.mixer.music.set_volume(0.03)
+    pygame.mixer.music.play(-1)
+
+music()
 
 run=True
 while run:                                          #loop หลักของเกมทำให้เกมทำงานได้
