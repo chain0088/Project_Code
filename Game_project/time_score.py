a7677c667661db5802f2b0ab5b771f68a3fc462a
Chatri-otto <oto77231@gmail.com>
2025-11-14 22:47:10 +0700

    Add files via upload

--- Game_project/time_score.py
+++ Game_project/time_score.py
@@ -12,6 +12,6 @@ def timer_score():
     pass_time = time.time() - start_time            #ตัวคำนวณเวลาที่จะใช้แสดงภายในเกม
     font = pygame.font.SysFont("arial",30)          
     time_text = font.render(f"Time {(int(pass_time))//60}:{(int(pass_time))%60}", True, (255,255,255)) #แสดงเวลาออกทางหน้าจอ
-    ojd.screen.blit(time_text,(10,10))
+    ojd.screen.blit(time_text,(750,550))
     score = font.render(f"Score {int(ojd.count)}", True, (255,255,255))
     ojd.screen.blit(score,(10,40))
