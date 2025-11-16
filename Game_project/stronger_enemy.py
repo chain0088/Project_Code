3f68225ecd775164402b8e038ccfd654c4b241d4
Chain <rachatanan.nam@dome.tu.ac.th>
2025-11-16 16:24:13 +0700

    Update stronger_enemy.py

--- Game_project/stronger_enemy.py
+++ Game_project/stronger_enemy.py
@@ -1,7 +1,7 @@
 import pygame
-import bullet as bul
 import object_data as ojd
 
 def Str_enemy():
     if ojd.count <= 10:
         ojd.enemy = pygame.transform.scale(ojd.enemy,(100-(ojd.down_size_enemy*ojd.count),100-(ojd.down_size_enemy*ojd.count)))
+
